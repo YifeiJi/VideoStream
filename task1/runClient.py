8571c06e9c692f312f0b56862577764dfcd73f63
jyf <jiyf990330@163.com>
2019-12-07 23:36:26 +0800

    refactor

--- task1/runClient.py
+++ task1/runClient.py
@@ -4,9 +4,10 @@ from Client import Client
 
 if __name__ == "__main__":
     try:
-        server_addr = sys.argv[1]
-        server_port = sys.argv[2]
-        my_port = sys.argv[3]
+        my_port = sys.argv[1]
+        server_addr = sys.argv[2]
+        server_port = sys.argv[3]
+
     except:
         print ("Error\n")
 
